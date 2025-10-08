ecd72579a564fd480962fe4bf44c04fffe135e97
chibu <>
2025-10-08 11:30:09 +0000

    remove more print

--- src/arenahard/show_result.py
+++ src/arenahard/show_result.py
@@ -118,7 +118,7 @@ def print_leaderboard(battles, category):
     _leaderboard = format_confidence_interval(mean_scores, lower_scores, upper_scores, baseline)
     
     print(f"##### Category: {category} #####")
-    print(_leaderboard.to_string())
+    #print(_leaderboard.to_string())
         
 
 def print_leaderboard_with_style_features(battles, benchmark, category,control_features):        
@@ -218,7 +218,7 @@ def print_leaderboard_with_style_features(battles, benchmark, category,control_f
     )
 
     print(f"##### Category: {category} #####")
-    print(_leaderboard.to_string())
+    #print(_leaderboard.to_string())
     print(f"Feature Coefs: {torch.quantile(coefs[:, -num_features:], 0.5, axis=0)}")
 
 
