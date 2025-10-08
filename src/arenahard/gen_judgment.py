3cc738f63806343a23a8e66524f11f46d928077f
chibu <>
2025-10-08 07:39:56 +0000

    add the answer keys

--- src/arenahard/gen_judgment.py
+++ src/arenahard/gen_judgment.py
@@ -177,7 +177,8 @@ def run (setting_file, endpoint_file, config_path, question_path, answer_path):
                 if model in existing_judgments and uid in existing_judgments[model]:
                     count += 1
                     continue
-                #print(f"The model answers: {model_answers}")
+                print(f"The model answers: {model_answers}")
+                print(f"The model answer keys: {model_answers.keys()}")
 
                 kwargs["answer"] = model_answers[model][uid]
                 kwargs["baseline"] = model_answers[
