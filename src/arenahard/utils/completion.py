422d1672c06906680c862fee0f79ed7f7e746e33
chibu <>
2025-11-19 01:28:10 +0000

    change timeout

--- src/arenahard/utils/completion.py
+++ src/arenahard/utils/completion.py
@@ -126,8 +126,9 @@ def chat_completion_openai(model, messages, temperature, max_tokens, api_dict=No
             base_url=api_dict["api_base"],
             api_key=api_dict["api_key"],
             #timeout = 10800.0,
-            timeout=httpx.Timeout(120.0, read=50.0, write=30.0, connect=30.0),
+            #timeout=httpx.Timeout(120.0, read=50.0, write=30.0, connect=30.0),
         )
+        print(client.timeout)
     else:
         client = openai.OpenAI()
         
